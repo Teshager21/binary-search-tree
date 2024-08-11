4b65c7c8732af5f2131346f4b1a4b03f5b8290c3
teshager <teshager8922@gmail.com>
2024-08-11 22:10:37 +0300

    add a recursive method of node insertion for building the tree

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -6,5 +6,50 @@ class Node{
     }
 }
 
-const node = new Node('Apple');
-console.log(node);
+class Tree{
+    constructor(array){
+        this.buildTree(array);
+
+    }
+root=new Node();
+    insertNode(rt,node ){
+        // console.log(rt)
+       if(rt!==null && rt.value===undefined){
+        // console.log('been here',rt)
+            Object.assign(rt,node);
+            rt.right= new Node();
+            rt.left= new Node();
+       } 
+       else { 
+        if(rt===null) {
+            Object.assign(rt,node)
+            //  console.log('heeeeeeeeeere',rt)
+            return rt
+        }
+        else if(node.value>rt.value){
+            this.insertNode(rt.right,node);
+            // console.log('root',this.root,rt.right);
+        }else {
+            this.insertNode(rt.left,node);
+            // console.log('root left',this.root,rt.left)
+        }
+       }
+    }
+    buildTree(array){
+    array.sort((a,b)=>a-b); //sort the array
+    array=[...new Set(array)];   //remove duplicates
+    // console.log(array)
+    //build nodes from the sorted array
+    array.forEach(element => {
+        const node=new Node(element);
+        this.insertNode(this.root,node);
+    });
+    //connect them together to build a tree
+    }
+
+}
+
+// const node = new Node('Apple');
+// console.log(node);
+
+export default Tree;
